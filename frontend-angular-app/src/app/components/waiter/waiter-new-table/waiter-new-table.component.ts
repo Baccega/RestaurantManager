c5bb0eba512982a3ac873eadeea8b6599947b12e
Scod3lla <giovescodella@gmail.com>
2019-07-09 16:36:26 +0200

    new-table :construction:

--- frontend-angular-app/src/app/components/waiter/waiter-new-table/waiter-new-table.component.ts
+++ frontend-angular-app/src/app/components/waiter/waiter-new-table/waiter-new-table.component.ts
@@ -1,27 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Dish } from "src/app/models/Dish";
+import { DishService } from "src/app/services/dish.service";
+import { OrderDetailComponent } from "../../order-detail/order-detail.component";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-waiter-new-table',
-  templateUrl: './waiter-new-table.component.html',
-  styleUrls: ['./waiter-new-table.component.scss']
+  selector: "app-waiter-new-table",
+  templateUrl: "./waiter-new-table.component.html",
+  styleUrls: ["./waiter-new-table.component.scss"]
 })
 export class WaiterNewTableComponent implements OnInit {
-  menu = [
-    {
-      cat: "anti",
-      piatti: ["ant1", "ant2", "ant3"]
-    },
-    {
-      cat: "primi",
-      piatti: ["prim1", "prim2", "prim3"]
-    },
-    {
-      cat: "secondi",
-      piatti: ["second1", "second2", "second3"]
+  menu: Dish[];
+
+  constructor(private dishService: DishService) {}
+
+  ngOnInit() {
+    this.getMenu();
+  }
+
+  getMenu(): void {
+    this.dishService.getMenu().subscribe(menu => (this.menu = menu));
+  }
+
+  addQuantity(elem) {
+    this.menu[elem].quantity += 1;
+    console.log(elem);
+  }
+  removeQuantity(elem) {
+    if (this.menu[elem].quantity > 0) this.menu[elem].quantity -= 1;
+    console.log(elem);
+  }
+
+  sendOrder():void {
+    let order: Dish[] = [];
+    for (let elem of this.menu) {
+      if (elem.quantity > 0) order.push(elem);
     }
-  ];
-  ngOnInit (){
-    
+    console.log(order);
+    this.dishService.sendOrder(order);
   }
 }
--- frontend-angular-app/src/app/services/dish.service.ts
+++ frontend-angular-app/src/app/services/dish.service.ts
@@ -1,29 +1,69 @@
 import { Injectable } from "@angular/core";
-import { stringify } from "@angular/compiler/src/util";
+import { Dish } from '../models/Dish';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: "root"
 })
-enum Categories {
-  antipasti,
-  primi,
-  secondi,
-  dolci,
-  bevande
-}
+
 
 export class DishService {
+
+  ordersDish:Dish[][]=[];
+
   constructor() {}
 
-  getMenu() {
-    return [
+  getMenu():Observable<Dish[]> {
+    return of([
       {
-        category: Categories.antipasti,
+        id:"0",
+        category: "antipasti",
         name: "baccala ecca la",
         price: 33,
         quantity: 0,
         done: false
       },
-    ];
+      
+      {
+        id:"1",
+        category: "primi",
+        name: "pasta con TONNOH",
+        price: 33,
+        quantity: 0,
+        done: false
+      },
+      
+      {
+        id:"2",
+        category: "secondi",
+        name: "pizza margherita",
+        price: 33,
+        quantity: 0,
+        done: false
+      },
+      
+      {
+        id:"3",
+        category: "dolci",
+        name: "panna e cioccolato",
+        price: 33,
+        quantity: 0,
+        done: false
+      },
+      
+      {
+        id:"4",
+        category: "bevande",
+        name: "juicy fruit mix",
+        price: 33,
+        quantity: 0,
+        done: false
+      }
+      
+    ]);
+  }
+
+  sendOrder(order:Dish[]){
+    this.ordersDish.push(order);
   }
 }
