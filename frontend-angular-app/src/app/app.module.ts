06ae3e66769afbce3a3003801db1aa3548473678
Sandro Baccega <sandrobac@hotmail.it>
2019-07-09 10:57:12 +0200

    Added waiter routing

--- frontend-angular-app/src/app/app.module.ts
+++ frontend-angular-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
-import { WaiterDashboardComponent } from './components/waiter/waiter-dashboard/waiter-dashboard.component';
-import { ChefDashboardComponent } from './components/chef/chef-dashboard/chef-dashboard.component';
-import { BartenderDashboardComponent } from './components/bartender/bartender-dashboard/bartender-dashboard.component';
-import { CashierDashboardComponent } from './components/cashier/cashier-dashboard/cashier-dashboard.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { WaiterDashboardComponent } from "./components/waiter/waiter-dashboard/waiter-dashboard.component";
+import { ChefDashboardComponent } from "./components/chef/chef-dashboard/chef-dashboard.component";
+import { BartenderDashboardComponent } from "./components/bartender/bartender-dashboard/bartender-dashboard.component";
+import { CashierDashboardComponent } from "./components/cashier/cashier-dashboard/cashier-dashboard.component";
+import { WaiterNewTableComponent } from "./components/waiter/waiter-new-table/waiter-new-table.component";
+import { WaiterComponent } from './components/waiter/waiter/waiter.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +20,12 @@ import { CashierDashboardComponent } from './components/cashier/cashier-dashboar
     WaiterDashboardComponent,
     ChefDashboardComponent,
     BartenderDashboardComponent,
-    CashierDashboardComponent
+    CashierDashboardComponent,
+    WaiterNewTableComponent,
+    WaiterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend-angular-app/src/app/components/waiter/waiter-new-table/waiter-new-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waiter-new-table',
+  templateUrl: './waiter-new-table.component.html',
+  styleUrls: ['./waiter-new-table.component.scss']
+})
+export class WaiterNewTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
