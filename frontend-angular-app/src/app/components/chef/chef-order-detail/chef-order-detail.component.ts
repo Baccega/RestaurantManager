3385a170e9ac8fc7898358e1bd1114cd6b8aeb5b
Sandro Baccega <sandrobac@hotmail.it>
2019-07-15 15:30:49 +0200

    Added redirect if order is finished :sparkles:

--- frontend-angular-app/src/app/components/chef/chef-order-detail/chef-order-detail.component.ts
+++ frontend-angular-app/src/app/components/chef/chef-order-detail/chef-order-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { DishStatus } from "src/app/models/Dish";
 import { Order } from "src/app/models/Order";
 import { OrderService } from "src/app/services/order.service";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { Subscription } from "rxjs";
 
 @Component({
@@ -17,7 +17,8 @@ export class ChefOrderDetailComponent implements OnInit, OnDestroy {
 
   constructor(
     private orderService: OrderService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -40,5 +41,11 @@ export class ChefOrderDetailComponent implements OnInit, OnDestroy {
 
   finishDish(index) {
     this.order.dishes[index].status = DishStatus["Finished"];
+    if (
+      this.order.dishes.filter(ord => ord.status != DishStatus["Finished"])
+        .length == 0
+    ) {
+      this.router.navigate(["/chef"]);
+    }
   }
 }
