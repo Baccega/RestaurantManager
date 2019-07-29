18b26a1e9a668b1baca421c7660fbc598394306b
Sandro Baccega <sandrobac@hotmail.it>
2019-07-29 17:28:28 +0200

    First draft cashier view-order :construction:

--- frontend-angular-app/src/app/components/cashier/cashier-view-order/cashier-view-order.component.ts
+++ frontend-angular-app/src/app/components/cashier/cashier-view-order/cashier-view-order.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
+import { UtilsService } from "src/app/services/utils.service";
+import { Subscription } from "rxjs";
+import { OrderService } from "src/app/services/order.service";
+import { Order } from "src/app/models/Order";
 
 @Component({
   selector: "app-cashier-view-order",
   templateUrl: "./cashier-view-order.component.html",
   styleUrls: ["./cashier-view-order.component.scss"]
 })
-export class CashierViewOrderComponent implements OnInit {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+export class CashierViewOrderComponent implements OnInit, OnDestroy {
+  tableId: string;
+  orders: Order[] = [];
+  routeSub: Subscription;
+  ordersSub: Subscription;
+  orderSub: Subscription[] = [];
 
-  ngOnInit() {}
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private utilService: UtilsService,
+    private orderService: OrderService
+  ) {}
+
+  ngOnInit() {
+    this.routeSub = this.activatedRoute.params.subscribe(params => {
+      this.tableId = params["table"];
+      this.utilService.setTitle(`Table: ${this.tableId}`);
+      this.ordersSub = this.orderService
+        .getOrders(this.tableId)
+        .subscribe(newOrders =>
+          newOrders.forEach((order, index) => {
+            this.orderSub[index] = this.orderService
+              .getOrder(order.id)
+              .subscribe(newOrder => (this.orders[index] = newOrder));
+          })
+        );
+    });
+  }
+
+  ngOnDestroy() {
+    this.orderSub.forEach(sub => sub.unsubscribe());
+    this.ordersSub.unsubscribe();
+    this.routeSub.unsubscribe();
+  }
 
   navigateToDashboard() {
     this.router.navigate(["cashier", "dashboard"]);
