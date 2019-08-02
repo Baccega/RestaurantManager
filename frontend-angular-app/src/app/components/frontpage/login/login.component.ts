098bbb0fd6e5652dd08967d3d532107332519eff
Sandro Baccega <sandrobac@hotmail.it>
2019-08-02 17:52:46 +0200

    Updated login and menu button

--- frontend-angular-app/src/app/components/frontpage/login/login.component.ts
+++ frontend-angular-app/src/app/components/frontpage/login/login.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormGroup
+} from "@angular/forms";
 import { LoginService } from "src/app/services/login.service";
 import { User } from "../../../models/User";
 import { Router } from "@angular/router";
@@ -13,9 +20,9 @@ import { UtilsService } from "src/app/services/utils.service";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  credential;
+  credential: FormGroup;
+
   constructor(
-    private formBuilder: FormBuilder,
     private loginService: LoginService,
     private router: Router,
     private _snackBar: MatSnackBar,
@@ -23,6 +30,7 @@ export class LoginComponent implements OnInit {
   ) {}
 
   onSubmit(data: any) {
+    console.log(this.credential);
     this.utilsService.setProgressbar(true);
 
     this.loginService.loginUser(data).subscribe(
@@ -41,14 +49,20 @@ export class LoginComponent implements OnInit {
         console.log(err.error);
       }
     );
-
-    this.credential.reset();
   }
 
   ngOnInit() {
-    this.credential = this.formBuilder.group({
-      email: "",
-      password: ""
+    this.utilsService.setTitle("Restaurant App");
+    this.credential = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(6)
+      ])
     });
   }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.credential.controls[controlName].hasError(errorName);
+  };
 }
--- frontend-angular-app/src/app/components/header/header.component.ts
+++ frontend-angular-app/src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Subscription } from "rxjs";
 import { UtilsService } from "src/app/services/utils.service";
+import { Router } from "@angular/router";
+import { Location } from "@angular/common";
 
 @Component({
   selector: "app-header",
@@ -12,10 +14,20 @@ export class HeaderComponent implements OnInit, OnDestroy {
   progressbar: boolean;
   titleSub: Subscription;
   progressSub: Subscription;
+  routerSub: Subscription;
+  notLogin: boolean;
 
-  constructor(private utilsService: UtilsService) {}
+  constructor(
+    private utilsService: UtilsService,
+    private location: Location,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.routerSub = this.router.events.subscribe(val => {
+      const loc = this.location.path();
+      this.notLogin = loc == "" || loc == "/login" ? false : true;
+    });
     this.titleSub = this.utilsService
       .watchTitle()
       .subscribe(title => (this.title = title));
@@ -25,6 +37,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.routerSub.unsubscribe();
     this.titleSub.unsubscribe();
     this.progressSub.unsubscribe();
   }
