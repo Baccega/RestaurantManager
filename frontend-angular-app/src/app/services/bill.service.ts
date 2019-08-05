b2ee5b2c70dc7554762fb435951020150184e5b0
Sandro Baccega <sandrobac@hotmail.it>
2019-08-05 12:08:21 +0200

    Cashier bill first draft :sparkles:

---
+++ frontend-angular-app/src/app/services/bill.service.ts
@@ -0,0 +1,162 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Order, FoodStatus, DrinkStatus } from "../models/Order";
+import { DishStatus } from "../models/Dish";
+import { Course } from "../models/Course";
+
+@Injectable({
+  providedIn: "root"
+})
+export class BillService {
+  orders: Course[] = [
+    {
+      id: "1",
+      name: "antipasti",
+      dishes: [
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "antipasti",
+          name: "baccala ecca la",
+          price: 33,
+          quantity: 1,
+          status: DishStatus["Finished"]
+        },
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "antipasti",
+          name: "baccala ecca la",
+          price: 33,
+          quantity: 2,
+          status: DishStatus["Finished"]
+        }
+      ]
+    },
+
+    {
+      id: "2",
+      name: "primi",
+      dishes: [
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "basta",
+          price: 33,
+          quantity: 1,
+          status: DishStatus["Finished"]
+        },
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "lisotto",
+          price: 33,
+          quantity: 1,
+          status: DishStatus["Finished"]
+        },
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "blodo",
+          price: 33,
+          quantity: 2,
+          status: DishStatus["Finished"]
+        }
+      ]
+    },
+    {
+      id: "2",
+      name: "primi",
+      dishes: [
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "basta",
+          price: 33,
+          quantity: 1,
+          status: DishStatus["Finished"]
+        },
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "lisotto",
+          price: 33,
+          quantity: 1,
+          status: DishStatus["Finished"]
+        },
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "blodo",
+          price: 33,
+          quantity: 2,
+          status: DishStatus["Finished"]
+        }
+      ]
+    },
+    {
+      id: "2",
+      name: "primi",
+      dishes: [
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "basta",
+          price: 33,
+          quantity: 1,
+          status: DishStatus["Finished"]
+        },
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "lisotto",
+          price: 33,
+          quantity: 1,
+          status: DishStatus["Finished"]
+        },
+        {
+          id: "0",
+          preparation: "10",
+          handler: "waiter1",
+          category: "primi",
+          name: "blodo",
+          price: 33,
+          quantity: 2,
+          status: DishStatus["Finished"]
+        }
+      ]
+    }
+  ];
+
+  constructor() {}
+
+  createBill(tableId) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    });
+  }
+
+  getBill(tableId): Observable<Course[]> {
+    return of(this.orders);
+  }
+}
