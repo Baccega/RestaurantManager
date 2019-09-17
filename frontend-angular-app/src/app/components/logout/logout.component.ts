7ebbb2128f4a19773e458a0060467b92c4fe2ec5
Sandro Baccega <sandrobac@hotmail.it>
2019-09-17 21:11:07 +0200

    Fixed login

--- frontend-angular-app/src/app/components/logout/logout.component.ts
+++ frontend-angular-app/src/app/components/logout/logout.component.ts
@@ -12,7 +12,14 @@ export class LogoutComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
   async ngOnInit() {
-    this.authService.logout();
+    this.authService.setUser({
+      role: "nobody",
+      userId: "0",
+      name: "",
+      dailyPlate: 0,
+      totalPlate: 0
+    });
+    sessionStorage.clear();
     this.router.navigate(["login"]);
   }
 }
