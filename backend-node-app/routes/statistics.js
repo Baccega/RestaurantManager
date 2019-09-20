acc21b766ab4bc9e2bbd8800cb2a924156a33d1f
Scod3lla <giovescodella@gmail.com>
2019-09-20 10:26:55 +0200

    adding reset daily plate

--- backend-node-app/routes/statistics.js
+++ backend-node-app/routes/statistics.js
@@ -2,39 +2,62 @@ var express = require("express");
 var router = express.Router();
 const UserModel = require("../models/users.models");
 const BillModel = require("../models/bills.models");
+const verify = require("./verifyToken");
 
-router.get("/daily", async (req, res, next) => {
-	try {
-		const now = new Date();
-		const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-		const found = await BillModel.find({ date: { $gte: today } });
+router.get("/daily", verify, async (req, res, next) => {
+  try {
+    const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const found = await BillModel.find({ date: { $gte: today } });
 
-		const response = found.reduce(
-			({ customersServed, profit }, bill) => ({
-				customersServed: customersServed + bill.customerNumber,
-				profit: profit + bill.dishes.reduce((sum, dish) => sum + dish.price, 0)
-			}),
-			{ customersServed: 0, profit: 0 }
-		);
+    const response = found.reduce(
+      ({ customersServed, profit }, bill) => ({
+        customersServed: customersServed + bill.customerNumber,
+        profit: profit + bill.dishes.reduce((sum, dish) => sum + dish.price, 0)
+      }),
+      { customersServed: 0, profit: 0 }
+    );
 
-		res.json(response);
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    res.json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get("/user/:id", async (req, res, next) => {
-	try {
-		const user = await UserModel.findOne({ userId: req.params.id });
+router.get("/user/:id", verify, async (req, res, next) => {
+  try {
+    const user = await UserModel.findOne({ userId: req.params.id });
 
-		const response = {
-			preparedDishes: user.role == "waiter" ? 0 : user.dailyPlate,
-			customersServed: user.role != "waiter" ? 0 : user.dailyPlate
-		};
-		res.json(response);
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    const response = {
+      preparedDishes: user.role == "waiter" ? 0 : user.dailyPlate,
+      customersServed: user.role != "waiter" ? 0 : user.dailyPlate
+    };
+    res.json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/closuer", verify, async (req, res, next) => {
+  try {
+    if (req.user.role != "cashier") {
+      res.status(400).send("Permission Denied!");
+    } else {
+      let users = await UserModel.find({});
+      console.log(users);
+
+      users.forEach(user => {
+        console.log(user);
+        user.totalPlate += user.dailyPlate;
+        user.dailyPlate = 0;
+      });
+      await users.save();
+
+      res.status(200).send("The resturant is close!");
+    }
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
 module.exports = router;
